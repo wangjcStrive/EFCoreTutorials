0a2cc0ee52c871ec167e51fa113a48397ce9cc54
wangjc857 <wangjc857@gmai.com>
2020-05-04 17:33:49 +0800

    Add project files.

---
+++ EFCoreTutorials/Program.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreTutorials.Model;
+using System;
+using System.Linq;
+
+namespace EFCoreTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new SchoolContext())
+            {
+                //var std = new Student()
+                //{
+                //    Name = "jicwang"
+                //};
+                //context.Students.Add(std);
+                //context.SaveChanges();
+                var query = context.Students.Where(s => s.Name == "jicwang").ToList();
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"jicwang -- {item.Name}");
+                }
+            }
+        }
+    }
+}
---
+++ EFCoreTutorials/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using EFCoreTutorials.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTutorials
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
